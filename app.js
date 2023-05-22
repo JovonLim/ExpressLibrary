bca20cf60b5f77bcfeb9d345972e7b03fdbc63c8
JovonLim <jovonlim@gmail.com>
2023-05-22 14:17:52 +0800

    Add env file for URI

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -10,7 +12,7 @@ const catalogRouter = require("./routes/catalog");
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "";
+const mongoDB = process.env.MONGODB_URI;
 
 var app = express();
 
